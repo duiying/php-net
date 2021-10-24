17afa06e1542378703150092e16965931d5d0916
wangyaxian <wangyaxiandev@gmail.com>
2021-10-24 18:26:00 +0800

    IO 多路复用之 select

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ require_once 'vendor/autoload.php';
 $server = new \PHPNet\Server('tcp://127.0.0.1:1234');
 // 监听
 $server->listen();
-// 等待客户端连接
-$server->accept();
+// IO 多路复用，处理多个客户端连接
+$server->eventLoop();
--- src/Server.php
+++ src/Server.php
@@ -13,6 +13,9 @@ class Server
         $this->localSocket = $localSocket;
     }
 
+    /**
+     * 监听
+     */
     public function listen()
     {
         // TCP
@@ -26,14 +29,63 @@ class Server
             echo '服务启动失败' . PHP_EOL;
             exit(0);
         }
+
+        echo sprintf('listen on %s' . PHP_EOL, $this->localSocket);
     }
 
+    /**
+     * 处理客户端连接
+     */
     public function accept()
     {
         $connectSocket = stream_socket_accept($this->serverSocket, -1);
         if ($connectSocket !== false) {
             echo '有客户端连接了' . PHP_EOL;
             $this->connections[(int)$connectSocket] = $connectSocket;
+        }
+    }
+
+    public function eventLoop()
+    {
+        while (1) {
+            // 监听 socket 只会有连接事件，所以只放入读监听数组中
+            $readSocketList         = [$this->serverSocket];
+            $writeSocketList        = [];
+            $exceptionSocketList    = [];
+
+            // 将连接 socket 放入读、写监听数组中
+            if (!empty($this->connections)) {
+                foreach ($this->connections as $k => $connectSocket) {
+                    $readSocketList[]   = $connectSocket;
+                    $writeSocketList[]  = $connectSocket;
+                }
+            }
+
+            $changedSocketCount = stream_select($readSocketList, $writeSocketList, $exceptionSocketList, null, null);
+            if ($changedSocketCount === false) {
+                echo '发生错误了' . PHP_EOL;
+                break;
+            }
+
+            echo '1' . PHP_EOL;
+
+            // 如果有了可读 socket
+            if (!empty($readSocketList)) {
+                foreach ($readSocketList as $k => $readSocket) {
+                    // 如果是监听 socket 可读，说明是有了客户端连接
+                    if ($readSocket === $this->serverSocket) {
+                        $this->accept();
+                    }
+                    // 如果不是监听 socket 可读，说明是客户端发来了数据
+                    else {
+                        $data = fread($readSocket, 1024);
+                        if (!empty($data)) {
+                            echo sprintf('收到了客户端发来的数据：%s' . PHP_EOL, $data);
+                            fwrite($readSocket, 'pong');
+                        }
+                    }
+                }
+            }
         }
     }
 }
