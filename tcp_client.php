adba81f366c3bc29d2e35dee8960002b285387c6
wangyaxian <wangyaxiandev@gmail.com>
2021-11-02 20:49:28 +0800

    fix tcp client

--- tcp_client.php
+++ tcp_client.php
@@ -1,10 +1,18 @@
 <?php
 
+/**
+ * 检查服务端心跳机制用
+ */
+
 $client = socket_create(AF_INET, SOCK_STREAM, 0);
 
 if (socket_connect($client, '127.0.0.1', 1234)) {
     socket_write($client, 'ping', 4);
     echo "从服务端收到了数据：" . socket_read($client, 1024) . PHP_EOL;
 }
+
+sleep(8);
+socket_write($client, 'ping', 4);
+echo "从服务端收到了数据：" . socket_read($client, 1024) . PHP_EOL;
 
 socket_close($client);
